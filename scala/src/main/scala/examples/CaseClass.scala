075c6afec5cdae7d4c990480ebc13cb4825861c3
Piotr Dorobisz <piotr.dorobisz@basementcrowd.com>
2019-11-12 11:22:10 +0100

    Update case class example

--- scala/src/main/scala/examples/CaseClass.scala
+++ scala/src/main/scala/examples/CaseClass.scala
@@ -4,11 +4,26 @@ object CaseClass {
 
   case class Person(name: String, lastName: String, age: Int)
 
-  def main(args: Array[String]) {
-    val person = Person("Janek", "Kowalski", 15)
+  def main(args: Array[String]): Unit = {
+    patternMatchingExample()
+  }
 
-    val Person(n, _, a) = person // przypisanie wartosci z case klasy (pominiecie drugiego argumentu)
+  private def patternMatchingExample(): Unit = {
+    println("\n---pattern matching example---")
+    val person = Person("John", "Smith", 35)
+    val Person(n, _, a) = person // we can extract only some fields and omit others
+    println(s"$n: $a")
+  }
 
-    println( s"$n: $a")
+  private def inheritanceExample() = {
+    // case-to-case inheritance is not supported
+    //    println("\n---inheritance example---")
+    //    case class Employee(override val name: String, override val lastName: String, override val age: Int, position: String) extends Person(name, lastName, age)
+    //
+    //    val e1 = Employee("John", "Smith", 35, "teacher")
+    //    val e2 = Employee("John", "Smith", 35, "assistant")
+    //
+    //    println(e1)
+    //    println(e2)
   }
 }

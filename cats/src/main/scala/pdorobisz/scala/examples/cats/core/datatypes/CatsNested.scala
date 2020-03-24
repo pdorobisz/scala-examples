3111ba0837935778efd280f5a8684f52dbdc467b
Piotr Dorobisz <piotr.dorobisz@basementcrowd.com>
2020-03-24 15:11:00 +0100

    Uncomment example

--- cats/src/main/scala/pdorobisz/scala/examples/cats/core/datatypes/CatsNested.scala
+++ cats/src/main/scala/pdorobisz/scala/examples/cats/core/datatypes/CatsNested.scala
@@ -9,9 +9,9 @@ import cats.implicits._
   */
 object CatsNested extends App {
 
-//  val optionList: Option[List[Int]] = Some(List(1, 2, 3, 4, 5))
-//  val n = Nested(optionList)
-//  val optionList2: Option[List[String]] = n.map(x => s"[$x]").value
-//
-//  println(optionList2)
+  val optionList: Option[List[Int]] = Some(List(1, 2, 3, 4, 5))
+  val n = Nested(optionList)
+  val optionList2: Option[List[String]] = n.map(x => s"[$x]").value
+
+  println(optionList2)
 }
